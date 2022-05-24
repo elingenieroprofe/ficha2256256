9f43fb1569dd5f635c639bca913809cb1af6c9f9
elingenieroprofe <65376162+elingenieroprofe@users.noreply.github.com>
2022-05-24 11:03:32 -0500

    pandersos

--- index.php
+++ index.php
@@ -5,10 +5,7 @@ hola conflic
 require_once 'Controller/user.controller.php';
 require_once 'Controller/Template.controller.php';
                 ///Model///
-require_once 'Model/dao/user.dao.php';
-require_once 'Model/dto/user.dto.php';
-            //////conexion/////
-require_once 'Model/conexion.php';
+requiro panderos
 
 
        //////// Arranque /////////
